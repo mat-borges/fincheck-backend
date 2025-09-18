2a35e943ed859f52d6bd84b8ee6a9cf50146f1a9
Mateus Borges <borgesm.work@gmail.com>
2025-09-18 19:08:23 -0300

    fix: types of categories and transactions

--- src/FinCheck.Domain/Models/Enums.cs
+++ src/FinCheck.Domain/Models/Enums.cs
@@ -4,14 +4,12 @@ namespace Fincheck.Domain.Models
     public enum TransactionType : byte
     {
         Income = 1,
-        Expense = 2,
-        Transfer = 3,
+        Expense = 2
     }
 
     public enum CategoryType : byte
     {
         Income = 1,
-        Expense = 2,
-        Transfer = 3
+        Expense = 2
     }
 }
