f74794e4f7fad99acb6c2fddb5677312ce9856ba
Mateus Borges <borgesm.work@gmail.com>
2025-09-18 15:48:05 -0300

    fix: initialize UpdatedAt property with current UTC time in BaseEntity

--- src/FinCheck.Domain/Models/BaseEntity.cs
+++ src/FinCheck.Domain/Models/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace Fincheck.Domain.Models
     {
     	public Guid Id { get; set; }
     	public DateTime CreatedAt { get; set; }
-    	public DateTime UpdatedAt { get; set; }
+    	public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
     	public bool IsDeleted { get; set; } = false; // Soft delete
     }
