f61e2202207f8d8757d0fda7e93dd0fe130e4aad
Mateus Borges <borgesm.work@gmail.com>
2025-09-18 15:53:50 -0300

    feat: refactor Category and Transaction models to use byte for Type property

---
+++ src/FinCheck.Infrastructure/Migrations/20250918185205_RefactorCategoryAndTransactionTypes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinCheck.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorCategoryAndTransactionTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Type",
+                schema: "fdbo",
+                table: "Transactions",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Type",
+                schema: "fdbo",
+                table: "Categories",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                schema: "fdbo",
+                table: "Transactions",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                schema: "fdbo",
+                table: "Categories",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
